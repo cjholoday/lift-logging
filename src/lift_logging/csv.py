2c40f2c03ba7a8cc2c3d70ddc069329804eb9389
Colton Holoday <colton@Coltons-MacBook-Air.local>
2026-02-07 23:51:35 -0500

    Use nornalized code in csv output

--- src/lift_logging/csv.py
+++ src/lift_logging/csv.py
@@ -7,6 +7,6 @@ class CSVPrinter:
         for workout in workouts:
             for entry in workout.entries:
                 output_lines.append('{};{};{}\n'.format(workout.workout_date,
-                                                        entry.exercise_code,
+                                                        entry.normalized_exercise_code,
                                                         entry.data))
         return output_lines
